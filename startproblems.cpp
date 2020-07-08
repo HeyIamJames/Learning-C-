5443b0275367f74f7851549da0233a08ce5825d9
James Hemmaplardh <jameshemmaplardh@gmail.com>
2020-07-07 17:16:47 -0700

    Update startproblems.cpp

--- startproblems.cpp
+++ startproblems.cpp
@@ -23,3 +23,24 @@ int main()
 }
 
 int okay () { std::cout << "Hello World!"; }
+
+#include <iostream> // preprocessor directive
+
+using namespace std;
+
+int main()
+{
+    cout ;
+    int x = 14;
+    cout << "Hello world! Piu" << endl; // stannd out, << stream insertion operator
+    cout << "/" << endl;
+    cout << "/  |" << endl;
+    cout << 1 + 2 + x << endl;
+    return 0;
+}
+
+
+// part 2
+
+#include <iostream>
+
